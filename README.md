e8b2489d3f8577e83f649f4d39549fda58c0dad9
qorlgns1 <rlgns0610@gmail.com>
2022-05-29 12:42:40 +0900

    WebSocket만을 이용해서 가벼운 통신하기

---
+++ README.md
@@ -0,0 +1,3 @@
+# 줌 클론코딩
+
+줌 클론코딩 NodeJS, WebRTC, Websockets 사용
---
+++ src/server.js
@@ -0,0 +1,42 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views/");
+app.use("/public", express.static(__dirname + "/public"));
+console.log("dirname", __dirname);
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+// app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "anonymous";
+  console.log("Connected to Brower ✅");
+  socket.on("close", () => console.log("Disconnected from the Brower ❌"));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+      default:
+        break;
+    }
+  });
+});
+
+server.listen(3000, handleListen);
